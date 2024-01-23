9aaab78457d5c59874341375d01518bd68949db6
adamogurcak <137228837+adamogurcak@users.noreply.github.com>
2024-01-23 16:57:10 +0100

    Zadani ukolu 1 cast prvni at to mam ulozene

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,79 @@
+import java.math.BigDecimal;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        public static void ukol1() {
+        System.out.println("Hello world!");
     }
-}
+    public static void ukol2() {
+        String uzivatel = "Karel";
+        System.out.println(uzivatel);
+    }
+
+    public static void ukol3() {
+        String pocetLekci = String.valueOf(10);
+        System.out.println(pocetLekci);
+    }
+
+    public static void ukol4() {
+        double cena = 10;
+        System.out.println(cena);
+    }
+
+    public static void ukol5() {
+        BigDecimal cena = BigDecimal.valueOf(0); // Nastavíme do proměnné hodnotu 0
+        // Desetkrát přičteme hodnotu jedna desetina (0,1):
+        for (int i = 0; i < 10; i++) {
+            cena = cena.add(BigDecimal.valueOf(0.1));
+               // operátor "+=" znamená: přičti ke stávající
+               // hodnotě navíc 0.1
+        }
+        System.out.println(cena);
+        // Má vypsat "1.0". Ale vypíše "0.9999" !!!
+
+    }
+
+    public static void ukol6() {
+        BigDecimal cena = BigDecimal.valueOf(0);
+        cena = cena.add(BigDecimal.valueOf(0.1));
+        System.out.println(cena);
+    }
+
+    public static void ukol7() {
+        int vykonMotoru = Integer.parseInt("120");
+        System.out.println("Výkon Motoru je: "+vykonMotoru+" kW.");
+    }
+
+    public static void ukol8() {
+        int velikostKosile = 37;
+        System.out.println("Velikost košile je: " + velikostKosile + ".");
+    }
+
+    public static void ukol9() {
+        System.out.println("Metoda *skoro* bez chybičky!");
+    }
+    public static void main(String[] args) {
+        System.out.println("Oprav chyby a spusť všechny metody!");
+        System.out.println("Komentáře odstraníš klávesovou "
+                +"zkratkou <Ctrl>+</> - použij lomítko "
+                +"na numerické klávesnici.");
+        ukol1();
+        ukol2();
+        ukol3();
+        ukol4();
+        ukol5();
+        ukol6();
+        ukol7();
+        ukol8();
+        ukol9();
+
+
+
+
+
+    }
+
+
+        }
+
